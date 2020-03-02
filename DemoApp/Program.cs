12363dbe712e184c9cac02e62474d00b90310907
Mike James <michael.james6@gmail.com>
2020-03-02 12:05:22 +0000

    Adding Demo console app

---
+++ DemoApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using MultimediaTimer;
+
+namespace DemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var timer = new Timer();
+            timer.Tick += Timer_Tick;
+            timer.Delay = TimeSpan.FromMilliseconds(5);
+            timer.Resolution = TimeSpan.FromMilliseconds(2);
+            timer.Start();
+
+            while (true)
+            {
+                string line = Console.ReadLine(); 
+                if (line == "exit") 
+                {
+                    timer.Stop();                   
+                    break;
+                }  
+            }
+        }
+
+        private static void Timer_Tick(object sender, EventArgs e)
+        {
+            Console.WriteLine($"Timer Ticked: {DateTime.Now.ToString("hh:mm:ss.fff")}");
+        }
+    }
+}
